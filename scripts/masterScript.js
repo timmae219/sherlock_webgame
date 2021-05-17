70500447fbad107fa789cc7ec5faab244463a83a
Timucin Maurice Erdar <t.erdar@cupofsoftware.de>
2021-05-17 19:06:18 +0200

    Initiales Projektgerüst aufgesetzt

---
+++ scripts/masterScript.js
@@ -0,0 +1,9 @@
+/*
+    Hier kommt das ganze Game Handling rein
+*/
+
+
+// Diese Funktion setzt den Content, der im Panel angezeigt werden soll
+function setGamePanelContent(content){
+    document.getElementById("gamepanel").innerHTML= content;
+}
