49ce0832cbf9990ad8cea2340052ef7bcf5cb846
Timucin Maurice Erdar <t.erdar@cupofsoftware.de>
2021-06-24 19:17:46 +0200

    Skript zur zufälligen Event-Verteilung implementiert

--- scripts/masterScript.js
+++ scripts/masterScript.js
@@ -2,13 +2,32 @@ import ScreenController from "./screenController.js";
 
 import {introText1, introText2, introText3, introText4} from "./strings.js";
 
+
+
+/*
+    Die Audio-Objekte der Musikstücke werden hier global definiert, damit man sie von überall starten und pausieren kann.
+    Effekte werden "lokal" behandelt, da sie meistens nur kurz sind und nicht manuell gemutet werden müssen
+*/
+
+var mainTheme = new Audio('../assets/audio/background_1.mp3');
+
 /*
     Hier kommt das ganze Game Handling rein
 
     Zu Debugzwecken kann man den initialen Screen hier abändern, damit man nicht immer bis zum gewünschten Punkt "spielen" muss.
 */
 var screenController = new ScreenController('intro');
+
+// Zuordnung von Events/Rätseln/Spielen zu den Häusern (Wald und Cabanon sind fixe Events!)
+var houseEventMapping = {};
+
+/*
+    Spiel-Kontrollfluss
+*/
 handleIntroScreen();
+initializeHouseEventMapping();
+
+console.log(houseEventMapping);
 
 
 // Diese Funktion setzt den Content, der im Panel angezeigt werden soll
@@ -16,13 +35,12 @@ function setGamePanelContent(screenName){
     screenController.currentScreen = screenName;
     document.getElementById("gamepanel").innerHTML= screenController.getScreenContent();
     if(screenName === 'intro'){
-        // TODO: irgendwie hauen die Animationen noch nicht so ganz hin.
         document.getElementById('status-panel').style.visibility = "hidden";
         document.getElementById('intro-panel').classList.add('animation');
         document.getElementById('intro-panel').innerHTML = `<p>${introText1}</p>`;
         document.getElementById('intro-panel').onclick = function(){
             document.getElementById('intro-panel').classList.remove('animation');
-            void document.getElementById('intro-panel').offsetWidth;
+            void document.getElementById('intro-panel').offsetWidth;    // Zeitliche Lückenfüller-Funktion, die das erneute Hinzufügen einer Klasse "sichtbar" wirksam macht
             document.getElementById('intro-panel').classList.add('animation');
             document.getElementById('intro-panel').innerHTML = `<p>${introText2}</p>`;
             document.getElementById('intro-panel').onclick = function(){
@@ -70,15 +88,22 @@ function handleIntroScreen(){
 
 function gotoMainScreen(){
     setGamePanelContent('main');
-    var audio = new Audio('../assets/audio/background_1.mp3');
-    audio.loop = true;
-    audio.play();
+    mainTheme.loop = true;
+    mainTheme.play();
 }
 
+// Bei Wald Zeit stoppen!
 function handleClickOnLocation(locationName){
     if(locationName.includes('house')){
         let effect = new Audio('../assets/audio/opening_door_sound_effect.mp3');
         effect.play();
+    }
+    if(locationName === 'cabanon'){
+        // TODO: Knarzende Bretter-Soundeffekt
+        // TODO: morsche Holzwand-Hintergrund
+    }
+    if(locationName === 'forest'){
+        // TODO: Blätterrascheln-Soundeffekt
     }
 }
 
@@ -88,4 +113,16 @@ function handleClickOnShowCandidates(){
 
 function handleClickOnSolve(){
     alert(`Du hast auf "Fall lösen" geklickt!`);
+}
+
+// Diese Funktion ordnet den Gebäuden zugehörige Events zu (jede Runde anders!) 
+function initializeHouseEventMapping(){
+    let houseNameList = ['house1', 'house2', 'house3', 'house4', 'house5', 'house6', 'house7', 'house8', 'house9', 'house10', 'house11', 'house12'];
+    let eventList = ['catch', 'catch', 'shoot', 'shoot', 'hat', 'hat', 'quiz', 'quiz', 'quiz', 'quiz', 'joker', 'joker'];   // joker -> man bekommt den Hinweis direkt
+
+    for(let house in houseNameList){
+        let randIndex = Math.floor(Math.random() * eventList.length);
+        houseEventMapping[house] = eventList[randIndex];
+        eventList.splice(randIndex, 1);
+    }
 }
