a11d67f0da449bfc2ae49604debcde31ca6ef2ee
Yannik_Juraschek <jurasche@students.uni-marburg.de>
2021-06-25 19:57:20 +0200

    erstes quiz

--- scripts/strings.js
+++ scripts/strings.js
@@ -5,4 +5,11 @@ const introText2 = "Du als Detektiv hast die Aufgabe, diesen Fall zu lösen.";
 const introText3 = "Befrage die Menschen im Dorf um Hinweise zum Verdächtigen zu bekommen.";
 const introText4 = "Aber Vorsicht! Stecke nicht die falsche Person in den Knast!";
 
-export  {introText1, introText2, introText3, introText4};
+
+const intro_rätsel_1 = "Was?! Ich soll dir helfen? Pff, ich habe selbst ein Rätsel zu lösen. Aber falls du mir bei meinem hilfst, so sage ich dir, was ich weiß.";
+const rätsel_1 = "Also es war so: Beim jährlichen Wagenrennen sind nur 4 Wagen ins Ziel gekommen: der blaue, der grüne, der rote und der weiße. Ich lag mit Schnupfen im Bett liegt und habe mich bei meinem Kumpel nach dem Ausgang des Rennens erkundigt. Doch er kannte nur Gerüchte: 1. Der blaue sei Vierter geworden. 2. Der grüne sei Erster geworden. 3. Der rote sei nicht Vierter geworden. 4. Der weiße sei weder Erster noch Vierter geworden. Von diesen Gerüchten sind drei wahr und eines ist falsch, das weiß ich. Nun muss ich herausfinden, welcher Wagen Erster und welcher Vierter geworden ist.";
+const rätsel_1_Möglichkeiten = "(A) der grüne und der blaue. (B) der grüne und der rote. (C) der weiße und der blaue. (D) der grüne und der weiße. (E) der rote und der blaue.";
+const rätsel_1_Lösung = "E";
+const rästel_1_Anwort = "Wow, ja super genau das ist die Lösung! Vielen Dank. Ich meine mich zu erinnern, dass ich eine Person mit + gesehen habe.";
+
+export  {introText1, introText2, introText3, introText4,intro_rätsel_1,rätsel_1,rätsel_1_Möglichkeiten,rätsel_1_Lösung,rästel_1_Anwort};
